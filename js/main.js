7c254751dcc159de60abf1325963aee1749cf99e
Bolaji Oyindamola <86451863+damolabolaji@users.noreply.github.com>
2022-09-20 18:50:47 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -2,9 +2,9 @@
 
 let total = 0
 document.querySelector('#pumpkin').addEventListener('click', makeZero)
-document.querySelector('#pumpkin').addEventListener('click', makeZero)
-document.querySelector('#pumpkin').addEventListener('click', makeZero)
-document.querySelector('#pumpkin').addEventListener('click', makeZero)
+document.querySelector('#dominosPizza').addEventListener('click', add3)
+document.querySelector('#zebra').addEventListener('click', add9)
+document.querySelector('#pumpkin').addEventListener('click', sub2)
 document.querySelector('#pumpkin').addEventListener('click', makeZero)
 
 function makeZero(){
@@ -17,3 +17,11 @@ function add3(){
 document.querySelector('#placeToPutResult').innerText = total
 }
 
+function add9(){
+total = total + 9
+  document.querySelector('#placeToPutResult').innerText = total
+}
+function sub2(){
+total = total - 2
+  document.querySelector('#placeToPutResult').innerText = total
+}
