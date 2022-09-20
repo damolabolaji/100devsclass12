628c45ec7315d6d293d36ee50459720f9f4fade8
Bolaji Oyindamola <86451863+damolabolaji@users.noreply.github.com>
2022-09-20 18:30:01 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,2 +1,19 @@
 
 
+let total = 0
+document.querySelector('#pumpkin').addEventListener('click', makeZero)
+document.querySelector('#pumpkin').addEventListener('click', makeZero)
+document.querySelector('#pumpkin').addEventListener('click', makeZero)
+document.querySelector('#pumpkin').addEventListener('click', makeZero)
+document.querySelector('#pumpkin').addEventListener('click', makeZero)
+
+function makeZero(){
+  total = 0
+  document.querySelector('#placeToPutResult').innerText = total
+}
+
+function add3(){
+  total = total + 3
+document.querySelector('#placeToPutResult').innerText = total
+}
+
