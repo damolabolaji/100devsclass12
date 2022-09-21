aba0981b466320e7c4ca1d0b97713f336bdaf6b1
Bolaji Oyindamola <86451863+damolabolaji@users.noreply.github.com>
2022-09-21 23:20:25 +0100

    Update main.js
    added sub20 funtion

--- js/main.js
+++ js/main.js
@@ -4,8 +4,8 @@ let total = 0
 document.querySelector('#pumpkin').addEventListener('click', makeZero)
 document.querySelector('#dominosPizza').addEventListener('click', add3)
 document.querySelector('#zebra').addEventListener('click', add9)
-document.querySelector('#pumpkin').addEventListener('click', sub2)
-document.querySelector('#pumpkin').addEventListener('click', makeZero)
+document.querySelector('#hippo').addEventListener('click', sub2)
+document.querySelector('#africa').addEventListener('click', sub20)
 
 function makeZero(){
   total = 0
@@ -24,4 +24,9 @@ total = total + 9
 function sub2(){
 total = total - 2
   document.querySelector('#placeToPutResult').innerText = total
+}
+
+function sub20(){
+total = -20
+  document.querySelector(#placeToPutResult).innerText = total
 }
